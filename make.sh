344451b5ebe0b03d9f605b330c8d7762cb0a764f
Poki <51330236+PokiDokika@users.noreply.github.com>
2020-05-27 19:55:19 +0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,9 @@
+# Just a small script that builds a bootable ISO
+
 mkdir tmp
+
+# I just happen to store the cross-compiler in here :P
+# Check https://wiki.osdev.org/GCC_Cross-Compiler if you need one
 
 ~/Documents/i686-elf-4.9.1-Linux-x86_64/bin/i686-elf-as boot.asm -o tmp/boot.o
 ~/Documents/i686-elf-4.9.1-Linux-x86_64/bin/i686-elf-gcc -c kernel.c -o tmp/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
