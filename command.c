a0039af9ecb572f428f44a010c0991b0e66f5c71
w196 <w196@github.com>
2020-06-25 11:06:47 -0400

    oops

---
+++ command.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include "terminal.c"
+
+/* this is really bad 
+void cmd_run () {
+	extern char cmd[255];
+	printf ("\nran: "); printf (cmd);
+} */
+
+extern char cmd[255];
+void cmd_run () {
+
+}
