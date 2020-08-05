2b4b4bd999ab19e1da6b79a28e452a841a563c0e
bjrkk <cfxqpkjq@gmail.com>
2020-08-05 19:02:09 +0300

    Shell enhancement: add fg and bg commands

--- command.c
+++ command.c
@@ -3,6 +3,24 @@
 #include <stdint.h>
 #include "lib_c/stdio.h"
 #include "lib_c/string.h"
+#include "terminal.h"
+
+// [bjrkk] FIXME: implement it as part of lib_c maybe?
+unsigned long __aton(char* buffer)
+{
+    unsigned long buf_conversion = 0;
+
+    for (; *buffer; buffer++)
+    {
+        char c = *buffer - '0';
+        if (c < 0 || c > 9) continue;
+
+        buf_conversion *= 10;
+        buf_conversion += c;
+    }
+
+    return buf_conversion;
+}
 
 extern char cmd[255];
 void cmd_run() 
@@ -24,6 +42,16 @@ void cmd_run()
 		printf("Pong!\n");
 	else if (!strcmp(cmd_name, "echo"))
 		printf("%s\n", cmd_args);
+	else if (!strcmp(cmd_name, "fg"))
+	{
+		uint8_t new_color = (terminal_color & 0xF0) + (__aton(cmd_args) & 0x0F);
+		terminal_setcolor(new_color);
+	}
+	else if (!strcmp(cmd_name, "bg"))
+	{
+		uint8_t new_color = (terminal_color & 0x0F) + ((__aton(cmd_args) & 0x0F) << 4);
+		terminal_setcolor(new_color);
+	}
 	else
 		printf("Unimplemented command\n");
 	
